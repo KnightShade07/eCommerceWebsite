038dbc68e5d22af25e22d431b71b5f033d1d36bd
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-16 15:52:46 -0700

    Issue#2 create adb using entity framework (#7)
    * Created Product Class
    
    * DBContext Class completed
    
    * SQL Package Installed, DBContext Registered
    
    * Added EF Tools
    
    * Created Product Database

---
+++ eCommerceWebsite/Data/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using eCommerceWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceWebsite.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> dbContextOptions)
+            : base(dbContextOptions) {}
+
+        public DbSet<Product> Products { get; set; }
+
+        
+    }
+}
