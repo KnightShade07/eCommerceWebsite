0b97e58795bd9be833c2c1549e95fa4f92ff75c0
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-16 15:50:44 -0700

    Created Product Database

--- eCommerceWebsite/Startup.cs
+++ eCommerceWebsite/Startup.cs
@@ -27,13 +27,11 @@ namespace eCommerceWebsite
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<ProductContext>(AddSqlServer);
+            services.AddDbContext<ProductContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
-        private void AddSqlServer(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-        }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
