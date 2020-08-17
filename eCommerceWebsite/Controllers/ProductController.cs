1ed325fb358ad300eebdfe8d1525a0fb52af802e
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-16 17:46:21 -0700

    Added command to show all products

---
+++ eCommerceWebsite/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceWebsite.Data;
+using eCommerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceWebsite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //Get all products from DB.
+            List<Product> products = _context.Products.ToList();
+            
+            return View(products);
+        }
+    }
+}
