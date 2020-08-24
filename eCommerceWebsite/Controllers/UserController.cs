da951abeff923306a370fdbcf6ee45ebc906e4b0
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-24 15:53:01 -0700

    Added Custom Error Messages, Login Page checks for invalid credentials

--- eCommerceWebsite/Controllers/UserController.cs
+++ eCommerceWebsite/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using eCommerceWebsite.Data;
 using eCommerceWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceWebsite.Controllers
 {
@@ -48,6 +49,31 @@ namespace eCommerceWebsite.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            UserAccount account =
+                 await (from u in _context.UserAccounts
+                        where (u.Username == model.UsernameOrEmail
+                            || u.Email == model.UsernameOrEmail)
+                            && u.Password == model.Password
+                        select u).SingleOrDefaultAsync();
+            if (account == null)
+            {
+                ModelState.AddModelError(string.Empty, "Credentials were not found.");
+                return View(model);
+            }
+
+            //Log user into website
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
