327589b51ee00a75b038042407e773ab6ccd4cbc
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-24 13:35:53 -0700

    Issue#12 create registration page (#30)
    * Created a User Account Model
    
    * Added Model to Context, then Code First Migration
    
    * Created RegisterViewModel
    
    * Added Validation to Register Page with Layout Tweaks.
    
    * Add Registered user into DB

--- eCommerceWebsite/Data/ProductContext.cs
+++ eCommerceWebsite/Data/ProductContext.cs
@@ -14,6 +14,7 @@ namespace eCommerceWebsite.Data
 
         public DbSet<Product> Products { get; set; }
 
+        public DbSet<UserAccount> UserAccounts { get; set; }
         
     }
 }
---
+++ eCommerceWebsite/Models/UserAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceWebsite.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email: ")]
+        public string ConfirmEmail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 8, ErrorMessage ="Password must be between {2} and {1} characters long")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm Password: ")]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        [DataType(DataType.Date)] //Time is ignored.
+        public DateTime? DateOfBirth { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+    }
+}
