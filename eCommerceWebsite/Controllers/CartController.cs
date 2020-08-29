8e26a6fbee11c4db6749b59648b4b40f0f66d1e1
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-28 19:57:05 -0700

    Added Cart Link to Nav and Add to Cart Button for each product

---
+++ eCommerceWebsite/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceWebsite.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Add(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
